6e14da1a1f5f266bd4b8ada49028c9d69550cd75
kassiane.a.carvalho <kassiane.a.carvalho@accenture.com>
2020-12-02 14:33:58 -0300

    inicio de aplicação

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [lista, setLista]=useState([]);
+  function salvar(event){
+    event.preventDefault()
+    const item = {
+      atividade: event.target.elements.atividades.value,
+      done: false
+    }
+    setLista([...lista,item])
+    console.log(lista);
+  }
+  function deletar(item){
+    setLista(lista.filter((atividade,index)=>{
+      if (item !== index){
+        return atividade;
+      }
+    }));
+  }
+  function editar(item){
+    setLista(lista.map((atividade,index)=>{
+      if (item !== index){
+        return atividade;
+      }
+      else{
+        atividade.atividade = prompt("digite um novo nome")
+        return atividade;
+      }
+    }
+    ))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={salvar}>
+        <input type="text" name="atividades" placeholder="#"/>
+        <input type="submit" value="enviar"/>
+      </form>
+      <ul>
+        {lista.map((tarefa, index)=>{
+          return <li>{tarefa.atividade} <button onClick={()=>deletar(index)} id={index}>Deletar</button> <button onClick={()=>editar(index)}>Editar</button> </li>
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import './header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Anotações</h1>
+      <p>Lista de afazeres</p>
+    </header>
+  );
+}
+
+export default Header;
